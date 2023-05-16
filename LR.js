ddc7aed70f1d190a28117de301525693a36cce36
YGrout01 <107133154+YGrout01@users.noreply.github.com>
2023-05-16 16:05:04 +0530

    Add files via upload

---
+++ LR.js
@@ -0,0 +1,30 @@
+var img="";
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+
+    object_detector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting object!";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
+
+function preload(){
+    img=loadImage('P1.jpg');
+}
+
+function draw(){
+    image(img,0,0,640,420);
+}
---
+++ main.js
@@ -0,0 +1,16 @@
+function LR()
+{
+    window.location = "LR.html";
+}
+function DR()
+{
+    window.location = "DR.html";
+}
+function BR()
+{
+    window.location = "BR.html";
+}
+function SR()
+{
+    window.location = "SR.html";
+}
